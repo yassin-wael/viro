d9eb38a5beb2983bc1481b8902d56a869cbf70a5
Yassin Wael <ywael6019@gmail.com>
2023-09-11 21:27:12 +0300

    added alerts and js lines

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 document.getElementById("btn-h").onclick = function () {
     document.getElementById("div1").innerHTML = "Scroll down to be directed the form page"
+    console.log("first commit is done")
 }
+document.getElementById("home-form").onclick = function () {
+    confirm("You will be directed the form page")
+    console.log("confirm is done")
+}
+document.getElementById("rels").onclick = function () {
+    alert("this section is under delopment")
+    console.log("alert is done")
+}
+///////////////////////////////////
+// Start of VARIABLES
+var num1 = 21
+var num2 = 18
+var num3 = 20
+var name = "Div(s)"
+document.getElementById("div2").innerHTML = (num1 + num2)
+document.getElementById("div2").innerHTML = (typeof num1),
+console.log(num1 + num2 + name) , console.log(typeof num1 + name)
+document.getElementById("div2").innerHTML = (num1 + num2 + name)
+console.log(num1 +  num2)
+console.log(typeof name )
