968e053d896ef25142be83efaecc08e0448b23d9
Yassin Wael <ywael6019@gmail.com>
2023-09-14 15:14:57 +0300

    js modified

--- js/script.js
+++ js/script.js
@@ -16,26 +16,29 @@ var num1 = 21
 var num2 = 18
 var num3 = 20
 var name = "Div(s)"
-document.getElementById("div2").innerHTML = (num1 + num2)
-document.getElementById("div2").innerHTML = (typeof num1),
-console.log(num1 + num2 + name) , console.log(typeof num1 + name)
+// document.getElementById("div2").innerHTML = (num1 - num2)
+// document.getElementById("div2").innerHTML = (typeof num1),
+// console.log(num1 + num2 + name) , console.log(typeof num1 - name)
 document.getElementById("div2").innerHTML = (num1 + num2 + name)
-console.log(num1 +  num2)
-console.log(typeof name )
+// console.log(num1 +  num2)
+// console.log(typeof name )
 ////////////////////////////
 // START OF BOOLEAN
 
 var X = true
 var Y = false
-console.log(typeof x)
 console.log(typeof X)
 // //////// ///////////
 // START OF ARRAYS
 
 var demv = ["Yassin", "SH", "Wael", false , 21 , 0]
+// var demv1 = [prompt("your name"), prompt("your age"), prompt("your city")]
+document.getElementById("mainlgo").onclick = function (){
+    var demv1 = [prompt("Your name"), prompt("Your Age"), prompt("Your City")]
+    document.getElementById("div3").innerHTML = (demv1)
+}
+// document.getElementById("div2").innerHTML = (demv1)
 console.log(demv)
-console.log(typeof demv [4])
-console.log(typeof demv[3])
 /////////////////////////
 // START OF OBJECT
 var obj = {
