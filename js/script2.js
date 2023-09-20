3cce2531ed9183432fcd63bffa16227bc7bb4572
Yassin Wael <ywael6019@gmail.com>
2023-09-20 18:18:49 +0300

    added UIV.1 testing branch

--- js/script2.js
+++ js/script2.js
@@ -2,3 +2,11 @@ document.getElementById("submit").onclick = function () {
     document.getElementById("name").innerHTML = "hello"
     console.log("done")
 }
+var x = 40
+var y = 2
+var z = x * y
+console.log(x)
+document.getElementById("interv").innerHTML = (z)
+// ///////////////
+console.log(x++) // operation result 
+console.log(x)
